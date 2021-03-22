56762b236f2793a63b3ce260f59edb75745109f1
Ethan Young <ethan.young@nrel.gov>
2021-03-22 12:31:05 -0600

    latest changes

--- AutoAspen/run_ethanol_model.py
+++ AutoAspen/run_ethanol_model.py
@@ -11,41 +11,74 @@ from classes import Aspen, Excel
 # from pythoncom import CoInitialize
 # import win32com.client as win32
 
+
 def main():
 
+	# Set the input and output files/directories
 	aspenFile = os.path.abspath('DW1102A_AB.bkp')
-	outDir = 'ethanol_output'
+	excelFile = os.path.abspath('DW1102A_2016UPDATES.xlsm')
 
+	outDir = 'ethanol_output'
 	os.makedirs(outDir, exist_ok = True)
 
-
+	# ================================================================
+	# Create Aspen Plus and Excel communicators
+	# ================================================================
 	print('Opening Aspen Plus model... ', end='')
 	aspenModel = Aspen(aspenFile)
 	print('Success!')
 
+	print('Opening Excel calculator... ', end='')
+	excelModel = Excel(excelFile)
+	print('Success!')
 
+	# ================================================================
+	# Write backup file with value updates/replacements
+	# ================================================================
 	# aspenPath: str, path in ASPEN tree
 	# value: float or str, value to set
 	# ifFortran: bool, whether it is a Fortran variable
-	print('Changing values in model backup tree... ', end='')
+	# ve_params['aspenPath'] = [value, ifFortran]
 	ve_params = {}
-	ve_params['path/in/aspen/tree'] = 0.75
-	ve_params['path/in/aspen/tree2'] = 0.01
+	ve_params['path/in/aspen/tree'] = [0.75, False]
+	ve_params['path/in/aspen/tree2'] = [0.01, False]
 
-	# for key, value in ve_params.items():
-	# 	aspenModel.set_value(key, value, bool(False))
+	DUMMY_OPERATION = True
+
+	print('Changing values in model backup tree... ', end='')
+	for key, value in ve_params.items():
+		if not DUMMY_OPERATION:
+			aspenModel.set_value(key, value[0], value[1])
+		else:
+			pass
 	print('Success!')
 
+	# ================================================================
+	# Run the Aspen Plus model
+	# ================================================================
 	print('Running model... ', end='')
 	aspenModel.run_model()
 	print('Success!')
 
+	# ================================================================
+	# Create Aspen Plus and Excel communicators
+	# ================================================================
 	print('Saving model output... ', end='')
 	tmpFile = '%s/%s.bkp' % (outDir, 'temp_backup')
 	aspenModel.save_model(tmpFile)
 	print('Success!')
 
+	# ================================================================
+	# Create Aspen Plus and Excel communicators
+	# ================================================================
+	print('Running Excel analysis... ', end='')
+	if not DUMMY_OPERATION:
+		calculator.load_aspenModel(tmpFile)
+		calculator.run_macro('solvedcfror')
+	print('Success!')
+
 	aspenModel.close()
+	excelModel.close()
 
 
 
