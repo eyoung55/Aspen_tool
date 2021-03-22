831c870027d23f1674a452ace3dfbbcfa02e6eff
Ethan Young <ethan.young@nrel.gov>
2021-03-22 12:37:36 -0600

    latest changes

--- AutoAspen/run_ethanol_model.py
+++ AutoAspen/run_ethanol_model.py
@@ -45,7 +45,7 @@ def main():
 
 	DUMMY_OPERATION = True
 
-	print('Changing values in model backup tree... ', end='')
+	print('Changing values in model backup definition tree... ', end='')
 	for key, value in ve_params.items():
 		if not DUMMY_OPERATION:
 			aspenModel.set_value(key, value[0], value[1])
@@ -56,14 +56,14 @@ def main():
 	# ================================================================
 	# Run the Aspen Plus model
 	# ================================================================
-	print('Running model... ', end='')
+	print('Running Aspen Plus model... ', end='')
 	aspenModel.run_model()
 	print('Success!')
 
 	# ================================================================
 	# Create Aspen Plus and Excel communicators
 	# ================================================================
-	print('Saving model output... ', end='')
+	print('Saving current model definition... ', end='')
 	tmpFile = '%s/%s.bkp' % (outDir, 'temp_backup')
 	aspenModel.save_model(tmpFile)
 	print('Success!')
@@ -72,8 +72,8 @@ def main():
 	# Create Aspen Plus and Excel communicators
 	# ================================================================
 	print('Running Excel analysis... ', end='')
+	calculator.load_aspenModel(tmpFile)
 	if not DUMMY_OPERATION:
-		calculator.load_aspenModel(tmpFile)
 		calculator.run_macro('solvedcfror')
 	print('Success!')
 
@@ -81,78 +81,6 @@ def main():
 	excelModel.close()
 
 
-
-	# # CoInitialize()
-	
-	# print('Opening Aspen Plus communicator... ', end='')
-	# aspen = win32.Dispatch('Apwn.Document')
-	# print('Success!')
-
-	# print('Initializing from backup file... ', end='')
-	# aspen.InitFromArchive2(aspenFile)
-	# print('Success!')
-	
-	# print('Running model... ', end='')
-	# aspen.Reinit()
-	# aspen.Engine.Run2()
-	# print('Success!')
-
-	# # simResults = simulate_using_aspen(aspenModel
-	# aspen.close()
-
 if __name__ == "__main__":
     main()
-
-# if __name__ == '__main__':
-
-# 	# parser = argparse.ArgumentParser(description = 'This script estimates outputs according to varied variables in Aspen, values are enumerated (discrete) or given in range (continuous)')
-# 	# parser.add_argument('-o', '--outDir', type = str, required = True, help = 'output directory')
-# 	# parser.add_argument('-c', '--configFile', type = str, required = True, help = 'config file, .xlsx')
-# 	# parser.add_argument('-a', '--aspenFile', type = str, required = True, help = 'Aspen model file, .bkp')
-# 	# parser.add_argument('-e', '--calculatorFile', type = str, required = True, help = 'excel calculator file, .xlsm')
-# 	# parser.add_argument('-d', '--varType', type = str, required = True, choices = ['dis', 'con'], help = 'input data type in config file, "dis" for discrete, "con" for continuous')
-# 	# parser.add_argument('-n', '--nruns', type = int, required = False, help = '# of simulation runs')
-# 	# args = parser.parse_args()
-	
-# 	outDir = args.outDir
-# 	configFile = args.configFile
-# 	aspenFile = args.aspenFile
-# 	calculatorFile = args.calculatorFile
-# 	varType = args.varType
-# 	nruns = args.nruns
-	
-# 	os.makedirs(outDir, exist_ok = True)
-
-
-# 	# parse inputs and outputs
-# 	inputInfos, outputInfos = parse_config(configFile)
-	
-# 	if varType == 'dis':
-# 		inputData = extract_input_data(inputInfos)
-# 	else:
-# 		inputData = generate_input_data(inputInfos)
-	
-	
-# 	# run simulation with Aspen
-# 	try:
-# 		aspenModel = Aspen(aspenFile)
-# 		calculator = Excel(calculatorFile)
-
-# 		simResults = simulate_using_aspen(aspenModel, calculator, inputData, outputInfos, outDir, nruns)
-	
-# 	finally:
-# 		aspenModel.close()
-# 		calculator.close()
-
-	
-# 	# save and plot results
-# 	save_simulation_results(simResults, outDir)
-	
-# 	plot_hist(simResults, outputInfos[['Output', 'Unit']], outDir)
-	
-
-
-
-
-
 
